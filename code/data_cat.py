eedef55c26931b8335e8e4014ff25ea717efa40a
MellacE98 <mellace1998@gmail.com>
2022-11-24 00:20:49 +0100

    Updated basic API functions
    Updated the data manipulation functions

--- code/data_cat.py
+++ code/data_cat.py
@@ -3,18 +3,33 @@ TODO
 """
 
 import utils_cat
+import os
 
+VARIABLES_URL = 'https://analisi.transparenciacatalunya.cat/resource/4fb2-n3yi.json'
+STATIONS_URL = 'https://analisi.transparenciacatalunya.cat/resource/yqwd-vj5e.json'
+DATA_URL = 'https://analisi.transparenciacatalunya.cat/resource/nzvn-apee.json'
 
 def main():
     """
     TODO
     """
-    utils_cat.get_station()
-    utils_cat.get_variables()
-    print(utils_cat.get_values())
-    # utilsCat.getValues()
+    path = os.getcwd()
+    print(path)
+    json_data = utils_cat.get_information(STATIONS_URL)
+    utils_cat.save_json(os.path.join(path, 'data', 'stations.json'), json_data)
 
+    json_data = utils_cat.get_information(VARIABLES_URL)
+    utils_cat.save_json(os.path.join(path, 'data', 'variables.json'), json_data)
+
+    """
+    path = os.getcwd()
+    json_data = utils_cat.get_information(DATA_URL, {'$limit': 1000000}) #"data_lectura between '2015-01-10T12:00:00' and '2017-01-10T14:00:00'"
+    utils_cat.save_json(os.path.join(path, 'data', 'prueba.json'), json_data)
+
+    dict_data = utils_cat.process_data(os.path.join(path, 'data', 'prueba.csv'), json_data)    
+    utils_cat.save_csv(os.path.join(path, 'data', 'prueba.csv'), dict_data)
+    """
     return 0
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     main()
--- code/utils_cat.py
+++ code/utils_cat.py
@@ -2,65 +2,96 @@
 TODO
 """
 
+import os
 import json
 import requests
 import pandas as pd
 
-def get_variables():
+
+def create_directory(path):
+    """Creates a directory at path
+    
+    Keyword arguments:
+    path -- path in which the directory will be created
     """
-    TODO
-    """
-    url = 'https://analisi.transparenciacatalunya.cat/resource/4fb2-n3yi.json'
-    variables = requests.get(url, timeout=2.5)
-    if variables.ok:
-        with open('data/variables.json', 'w', encoding='UTF-8') as file:
-            json.dump(variables.json(), file)
-        return 1
-    return 0
+    print(path)
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        print('File "data" exisits')
 
 
-def get_station():
+def clean_directories(path):
+    """Deletes files inside data directory
+    
+    path -- path of the directory to clean
     """
-    TODO
+    for file in os.listdir(path):
+        f_path = os.path.join(path, file)
+        try:
+            os.remove(path=f_path)
+        except FileNotFoundError:
+            continue
+
+# https://dev.socrata.com/foundry/analisi.transparenciacatalunya.cat/nzvn-apee
+# https://dev.socrata.com/docs/queries/
+# https://dev.socrata.com/docs/datatypes/floating_timestamp.html#,
+def get_information(url, parameters={}):
+    """Requests information from a url
+
+    Keyword arguments:
+    url -- url to get the info
+    parameters -- dict with the params for the request
     """
-    url = 'https://analisi.transparenciacatalunya.cat/resource/yqwd-vj5e.json'
-    stations = requests.get(url, timeout=2.5)
-    if stations.ok:
-        with open('data/stations.json', 'w', encoding='UTF-8') as file:
-            json.dump(stations.json(), file)
-        return 1
-    return 0
+    data = requests.get(url, timeout=2.5, params=parameters)
+    if data.ok:
+        return data.json()
+    return None
 
 
-def get_values():
+def save_json(path, data):
+    """Saves .json file with the data
+
+    Keyword arguments:
+    path -- path in which the file will be saved
+    data -- json format data to save into a .json
+    """
+    with open(path, 'w', encoding='UTF-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+def save_csv(path, data):
+    """Saves .csv file with the data
+
+    Keyword arguments:
+    path -- path in which the file will be saved
+    data -- dict with data to save into a .csv
+    """
+    df = pd.DataFrame.from_dict(data, orient='index')
+    df.to_csv(path, index=True)
+    return None
+
+
+def process_data(path, data):
     """
     TODO
     """
     sample_dict = dict((el, -1) for el in create_variables_dict())
     w_dict = {}
-    limit, offset = 10000, 0
-    url = 'https://analisi.transparenciacatalunya.cat/resource/nzvn-apee.json'
-
-    weather = requests.get(url, timeout=2.5, params={'$offset': offset, '$limit': limit})
-    if weather.ok:
-        data = weather.json()
-        for obs in data:
-            id = obs['id'][:2] + obs['id'][4:]
-            variable = obs['codi_variable']
-            if id not in w_dict:
-                w_dict[id] = sample_dict.copy()
-                w_dict[id][variable] = obs['valor_lectura']
-        return pd.DataFrame(w_dict.items())
-    else:
-        return 0
+    for obs in data:
+        obs_id = obs['id'][:2] + obs['id'][4:]
+        variable = obs['codi_variable']
+        if obs_id not in w_dict:
+            w_dict[obs_id] = sample_dict.copy()
+        w_dict[obs_id][variable] = obs['valor_lectura']
+    return w_dict
 
 
 def create_variables_dict():
-    """
-    TODO
-    """
+    """Returns a dict where variable codes are keys and acronims the values"""
     v_dict = {}
     v_data = json.load(open('data/variables.json', encoding='UTF-8'))
     for var in v_data:
         v_dict[var['codi_variable']] = var['acronim']
     return v_dict
+    
